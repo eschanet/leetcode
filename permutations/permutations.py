ed9a5c2ef5f71f502ab8acd17c5bd8c9fd8244bd
Eric Schanet <eric.schanet@gmail.com>
2021-05-26 23:03:44 +0200

    Time: 32 ms (95.93%), Space: 14.4 MB (70.48%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,7 +1,23 @@
 import itertools
 
 class Solution:
+    
+    def my_permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) <= 1:
+            yield nums
+        else:
+            for permutation in self.my_permute(nums[1:]):
+                for i in range(len(nums)):
+                    yield permutation[:i] + nums[0:1] + permutation[i:]
+            
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return [list(p) for p in list(itertools.permutations(nums))]
         
+        # the lazy/smart way
+        # return [list(p) for p in list(itertools.permutations(nums))]
         
+        # the hard way
+        answer = []
+        for perm in self.my_permute(nums):
+            answer.append(perm)
+        return answer
+    
