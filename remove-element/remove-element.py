dc4dbd06a6d69c42c04fd488a5a4a93b2d377372
Eric Schanet <eric.schanet@gmail.com>
2021-06-29 15:43:39 +0200

    Time: 36 ms (49.08%), Space: 14.3 MB (12.89%) - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -4,5 +4,6 @@ class Solution:
         for i in range(len(nums)-1, -1, -1):
             if nums[i] == val:
                 del nums[i]
-                            
+        
+        # no need to pad the list, just return k
         return len(nums)
