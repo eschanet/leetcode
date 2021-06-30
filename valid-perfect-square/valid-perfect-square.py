4d6b00da4710fdad4e5e969f010a47b2feed66eb
Eric Schanet <eric.schanet@gmail.com>
2021-06-30 11:01:36 +0200

    Time: 28 ms (83.67%), Space: 14.3 MB (34.87%) - LeetHub

--- valid-perfect-square/valid-perfect-square.py
+++ valid-perfect-square/valid-perfect-square.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
+    def isPerfectSquare_babylonian(self, num: int) -> bool:
         guess = num / 2
         
         while abs(guess*guess - num) > 0.1:
@@ -9,3 +9,21 @@ class Solution:
             return True
     
         return False
+    
+    def isPerfectSquare_binary(self, num: int) -> bool:
+        low = 1
+        high = num
+        
+        while low <= high:
+            mid = int(0.5*(low + high))
+            if mid*mid < num:
+                low = mid + 1
+            elif mid*mid > num:
+                high = mid - 1
+            else:
+                return True
+            
+        return False
+    
+    def isPerfectSquare(self, num: int) -> bool:
+        return self.isPerfectSquare_binary(num)
