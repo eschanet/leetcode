e395459dcdef02107ca5e109d8b9ad5c6436e381
Eric Schanet <eric.schanet@gmail.com>
2021-06-30 09:47:49 +0200

    Time: 44 ms (15.17%), Space: 14.4 MB (33.48%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,18 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        # quick sanity check before we deconstruct the string
-        for o, c in [('(',')'), ('{','}'), ('[',']')]:
-            if (s.count(o) + s.count(c)) % 2 != 0: return False
-           
+                           
+        old = ""
+        while old != s:
+            old = s
+            s = s.replace('()','').replace('[]','').replace('{}','')
+            
+        if len(s) == 0: return True
+        return False
+    
+
+    def isValid_regex(self, s: str) -> bool:
         import re
         replacements = {"\{\}": "", "\[\]": "", "\(\)": "",}
         pattern = re.compile("|".join(replacements.keys()))
         
-        old, new = "", s
-        while old != new:
-            old = new
-            new = pattern.sub(lambda m: replacements[re.escape(m.group(0))], old)
-            
-        if len(new) == 0: return True
-        else: return False
+        old = ""
+        while old != s:
+            old = s
+            s = pattern.sub(lambda m: replacements[re.escape(m.group(0))], old)
+
+        if len(s) == 0: return True
+        return False
