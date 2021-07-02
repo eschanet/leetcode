8aa4e7be905fc2ee05576bef0452cce7397630eb
Eric Schanet <eric.schanet@gmail.com>
2021-07-02 14:57:26 +0200

    Time: 836 ms (17.82%), Space: 16.8 MB (6.32%) - LeetHub

--- degree-of-an-array/degree-of-an-array.py
+++ degree-of-an-array/degree-of-an-array.py
@@ -1,13 +1,31 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         
-        # contains right_index - left_index + 1, number_of_occurrences for each unique element in list
-        candidates = [
-            (
-                nums.count(n),
-                (len(nums) - nums[::-1].index(n) - nums.index(n))
-            )
-            for n in set(nums)
-        ]
+#         # contains right_index - left_index + 1, number_of_occurrences for each unique element in list
+#         candidates = [
+#             (
+#                 nums.count(n),
+#                 (len(nums) - nums[::-1].index(n) - nums.index(n))
+#             )
+#             for n in set(nums)
+#         ]
         
-        return min([cand[1] for cand in candidates if cand[0] == max(candidates)[0]])
+#         return min([cand[1] for cand in candidates if cand[0] == max(candidates)[0]])
+        
+        
+        # not as nice, but hopefully faster
+        occurrences = {}
+        indices = {}
+        for idx, n in enumerate(nums):
+            if n in occurrences:
+                indices[n].append(idx)
+                occurrences[n] += 1
+            else:
+                indices[n] = [idx]
+                occurrences[n] = 1
+        
+        return min([
+            idx[-1] - idx[0] + 1 
+            for n, idx in indices.items() 
+            if occurrences[n] == max(occurrences.values())
+        ])            
