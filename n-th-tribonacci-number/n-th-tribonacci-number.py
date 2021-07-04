4da6e14ed11fb555016f326151b67a0119080942
Eric Schanet <eric.schanet@gmail.com>
2021-07-04 18:43:46 +0200

    Time: 28 ms (79.86%), Space: 14.2 MB (43.15%) - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.py
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         meme = {}
-                
+        
         def trib_bottomup(n: int) -> int:
+            
             meme[0], meme[1], meme[2] = 0, 1, 1
+            
             for i in range(3, n+1):
                 meme[i] = meme[i-1] + meme[i-2] + meme[i-3] 
+            
             return meme[n]
         
         return trib_bottomup(n)
