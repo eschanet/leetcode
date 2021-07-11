7202b6ca3ecbd3a683abf76079888880c50b406b
Eric Schanet <eric.schanet@gmail.com>
2021-07-11 21:40:18 +0200

    Time: 8 ms (100.00%), Space: 14.3 MB (11.19%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,14 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
         if n == 1: 
             return 1
         elif n == 2:
             return 2
         
         first, second, third = 1, 2, 3
-        
-        
         for i in range(3,n+1):
             third = first + second
             first, second = second, third
