46c0d0eea00d7d23c79518385fef7d32f62903f5
Eric Schanet <eric.schanet@gmail.com>
2021-07-11 22:42:49 +0200

    Time: 32 ms (82.29%), Space: 14.1 MB (98.72%) - LeetHub

--- symmetric-tree/symmetric-tree.py
+++ symmetric-tree/symmetric-tree.py
@@ -7,14 +7,38 @@
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         
-        def left_right_symmetric(left: TreeNode, right: TreeNode) -> bool:
-            
+        if not root:
+            return True
+        
+        def is_symmetric_recursive(left: TreeNode, right: TreeNode) -> bool:
             if not left and not right:
                 return True
             elif not left or not right:
                 return False
             else:
                 return (left.val == right.val) and left_right_symmetric(left.left, right.right) and left_right_symmetric(left.right, right.left)
-
-        return left_right_symmetric(root.left, root.right)
+        
+        def is_symmetric_iterative(left: TreeNode, right: TreeNode) -> bool:
+            queue = [left, right]
+            
+            while len(queue) > 0:
+                left = queue.pop()
+                right = queue.pop()
+                
+                if not left and not right:
+                    continue  
+                elif not left or not right:
+                    return False
+                elif left.val != right.val:
+                    return False
+                
+                queue.append(left.left)
+                queue.append(right.right)
+                queue.append(left.right)
+                queue.append(right.left)
+            
+            return True
+                
+        # return is_symmetric_recursive(root.left, root.right)
+        return is_symmetric_iterative(root.left, root.right)
         
