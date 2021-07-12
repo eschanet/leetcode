3771ee2ca454fe4a798b46670376a12c53a4699c
Eric Schanet <eric.schanet@gmail.com>
2021-07-12 12:25:20 +0200

    Time: 9584 ms (5.02%), Space: 14.4 MB (62.26%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -9,10 +9,13 @@ class Solution:
         longest_palindrome = ""
         for i in range(len(s)):
             for j in range(len(s), -1 , -1):
-                if i >= j:
+                if i >= j-1:
                     continue
                 if is_palindrome(s[i:j]):
                     if len(s[i:j]) > len(longest_palindrome):
                         longest_palindrome = s[i:j]
-                        
+        
+        if longest_palindrome == "":
+            return s[0]
+        
         return longest_palindrome
