eb497619613d3121850e4d6242ae12cf6d86ef40
Eric Schanet <eric.schanet@gmail.com>
2021-07-28 18:59:14 +0200

    Time: 24 ms (98.02%), Space: 14.1 MB (93.57%) - LeetHub

--- divide-two-integers/divide-two-integers.py
+++ divide-two-integers/divide-two-integers.py
@@ -1,5 +1,6 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        
         positive = (dividend > 0) is (divisor > 0)
         divisor, dividend = abs(divisor), abs(dividend)
 
@@ -8,9 +9,11 @@ class Solution:
 
         while the_sum <= dividend:
             curr_frac = 1
+            
             while (the_sum + the_sum) <= dividend:
                 the_sum += the_sum
                 curr_frac += curr_frac
+            
             dividend -= the_sum
             the_sum = divisor
             frac += curr_frac
