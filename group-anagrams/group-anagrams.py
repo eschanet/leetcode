d6cfd5b25440c11fa24c17a8c80007ea00ec9de3
Eric Schanet <eric.schanet@gmail.com>
2021-08-02 22:15:22 +0200

    Time: 96 ms (78.37%), Space: 18 MB (39.78%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,16 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
-        while strs:
-            s = strs.pop()
-            print(s)
-            if str(sorted(s)) in anagrams:
-                anagrams[str(sorted(s))].append(s)
-            else:
-                anagrams[str(sorted(s))] = [s]
-        
-        ans = []
-        for key, l in anagrams.items():
-            ans.append(l)
-        
-        return ans
+        for s in strs:
+            key = tuple(sorted(s))
+            anagrams[key] = anagrams.get(key, []) + [s]
+
+        return anagrams.values()
